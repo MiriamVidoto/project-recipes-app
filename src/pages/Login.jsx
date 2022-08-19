d8460aa44a697886f6e21e95d3efa5444c76ea3e
leovcampos <leonardoviniciuscampos@gmail.com>
2022-08-19 20:26:03 -0300

    group 4 requisitos 2 ao 6 em andamento

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,30 +1,77 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
 import './style/login.css';
 
-function Login() {
+function Login({ history }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    const regex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    const passwordLength = 6;
+    if (!!regex.test(email) && password.length > passwordLength) {
+      return setIsDisabled(false);
+    }
+  }, [email, password]);
+
+  const handlePassword = ({ target }) => {
+    setPassword(target.value);
+  };
+
+  const handleEmail = ({ target }) => {
+    setEmail(target.value);
+  };
+
+  const routeFoods = () => {
+    history.push('/foods');
+  };
+
   return (
-    <div className="container">
-      <input
-        type="email"
-        className="inputLogin"
-        placeholder="Type your email..."
-        data-testid="email-input"
-      />
-      <input
-        type="password"
-        className="inputLogin"
-        placeholder="Type your password..."
-        data-testid="password-input"
-      />
+    <form className="container">
+      <label htmlFor="email">
+        Email:
+        <input
+          id="email"
+          type="email"
+          className="inputLogin"
+          placeholder="type your email..."
+          data-testid="email-input"
+          name="email"
+          value={ email }
+          onChange={ handleEmail }
+        />
+      </label>
+      <label htmlFor="password">
+        Password:
+        <input
+          id="password"
+          type="password"
+          className="inputLogin"
+          placeholder="type your password..."
+          data-testid="password-input"
+          name="password"
+          value={ password }
+          onChange={ handlePassword }
+        />
+      </label>
       <button
         type="button"
         className="buttonLogin"
         data-testid="login-submit-btn"
+        disabled={ isDisabled }
+        onClick={ routeFoods }
       >
         Enter
       </button>
-    </div>
+    </form>
   );
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;

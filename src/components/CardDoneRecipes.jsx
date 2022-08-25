063394cb67b3390bf9b9c3c22e747c5d3bd2612f
MiriamVidoto <miriamvidoto.hdg@gmail.com>
2022-08-25 13:55:51 -0300

    requisito 45 ok

--- src/components/CardDoneRecipes.jsx
+++ src/components/CardDoneRecipes.jsx
@@ -4,31 +4,36 @@ import shareIcon from '../images/shareIcon.svg';
 import './styles/CardDoneRecipes.css';
 
 function CardDoneRecipes() {
-  // const doneRecipes = [
-  //   { id: '52771',
-  //     type: 'foods',
-  //     nationality: 'Italian',
-  //     category: 'Vegetarian',
-  //     alcoholicOrNot: '',
-  //     name: 'Spicy Arrabiata Penne',
-  //     image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-  //     doneDate: '25/08/2022',
-  //     tags: ['Pasta', 'Curry', 'teste'],
-  //   },
-  //   { id: '178319',
-  //     type: 'drinks',
-  //     nationality: '',
-  //     category: 'Cocktail',
-  //     alcoholicOrNot: 'Alcoholic',
-  //     name: 'Aquamarine',
-  //     image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-  //     doneDate: '24/08/2022',
-  //     tags: [],
-  //   }];
+  const doneRecipes = [
+    {
+      id: '52771',
+      type: 'food',
+      nationality: 'Italian',
+      category: 'Vegetarian',
+      alcoholicOrNot: '',
+      name: 'Spicy Arrabiata Penne',
+      image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+      doneDate: '23/06/2020',
+      tags: ['Pasta', 'Curry'],
+    },
+    {
+      id: '178319',
+      type: 'drink',
+      nationality: '',
+      category: 'Cocktail',
+      alcoholicOrNot: 'Alcoholic',
+      name: 'Aquamarine',
+      image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+      doneDate: '23/06/2020',
+      tags: [],
+    },
+  ];
 
-  const doneRecipes = localStorage.getItem('doneRecipes')
-    ? JSON.parse(localStorage.getItem('doneRecipes'))
-    : [];
+  console.log(doneRecipes);
+
+  // const doneRecipes = localStorage.getItem('doneRecipes')
+  //   ? JSON.parse(localStorage.getItem('doneRecipes'))
+  //   : [];
 
   const LIMIT_TAGS = 2;
 
@@ -37,7 +42,7 @@ function CardDoneRecipes() {
       {
         doneRecipes.map((element, index) => (
           <div className="card-done-recipes" key={ index }>
-            <Link to={ `/${element.type}/${element.id}` }>
+            <Link to={ `/${element.type}s/${element.id}` }>
               <img
                 src={ element.image }
                 alt={ element.name }
@@ -48,7 +53,7 @@ function CardDoneRecipes() {
             <div className="container-done-recipes">
               <div className="share">
                 {
-                  element.type === 'foods' ? (
+                  element.type === 'food' ? (
                     <p data-testid={ `${index}-horizontal-top-text` }>
                       { `${element.nationality} - ${element.category}` }
                     </p>
@@ -68,7 +73,7 @@ function CardDoneRecipes() {
                   className="share-icon"
                 />
               </div>
-              <Link to={ `/${element.type}/${element.id}` }>
+              <Link to={ `/${element.type}s/${element.id}` }>
                 <h3 data-testid={ `${index}-horizontal-name` }>
                   {element.name }
                 </h3>

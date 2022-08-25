a608b98844e2443d9957e05b1d2aeb51005e7705
Rodrigo Sena <dias.rodrigosena@gmail.com>
2022-08-25 19:33:31 -0300

    Req 37

--- src/components/RecipeInProgress.jsx
+++ src/components/RecipeInProgress.jsx
@@ -23,6 +23,10 @@ function RecipeInProgress({ type }) {
     .filter((key) => key.includes('strMeasure'))
     : [];
 
+  const listIngredients = ingredientes
+    .filter((e) => recipe[0][e] !== null)
+    .filter((ele) => recipe[0][ele].length !== 0);
+
   useEffect(() => {
     getRecipeAPI();
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -58,30 +62,21 @@ function RecipeInProgress({ type }) {
               Favoritar
             </button>
             <p data-testid="recipe-category">{recipe[0].strCategory}</p>
-            { type === 'meal'
-              && ingredientes.filter((element) => recipe[0][element].length !== 0)
-                .map((ingrediente, index) => (
+            <ul>
+              {
+                listIngredients.map((ingrediente, index) => (
                   <li
                     key={ index }
                     data-testid={ `${index}-ingredient-step` }
+                    id="li-ingredients"
                   >
                     {recipe[0][ingrediente]}
                     {' - '}
                     {recipe[0][amount[index]]}
                   </li>
-                ))}
-            { type === 'cocktails'
-              && ingredientes.filter((element) => recipe[0][element] !== null)
-                .map((ingredient, index) => (
-                  <li
-                    key={ index }
-                    data-testid={ `${index}-ingredient-step` }
-                  >
-                    {recipe[0][ingredient]}
-                    {' - '}
-                    {recipe[0][amount[index]]}
-                  </li>
-                ))}
+                ))
+              }
+            </ul>
             <h3>Instructions</h3>
             <p
               data-testid="instructions"
